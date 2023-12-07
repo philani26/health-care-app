f98eb3c612bc788ed3e6b557b51b179b97b8a35a
philani26 <gumedephilani9@gmail.com>
2023-12-07 12:35:38 +0000

    Code Synced by DhiWise

---
+++ android/app/src/main/kotlin/com/healtcareapp/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.healtcareapp.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/app_export.dart
@@ -0,0 +1,12 @@
+export 'package:healt_care_app/core/constants/constants.dart';
+export 'package:healt_care_app/core/utils/image_constant.dart';
+export 'package:healt_care_app/core/utils/navigator_service.dart';
+export 'package:healt_care_app/core/utils/size_utils.dart';
+export 'package:healt_care_app/localization/app_localization.dart';
+export 'package:healt_care_app/routes/app_routes.dart';
+export 'package:healt_care_app/theme/app_decoration.dart';
+export 'package:healt_care_app/theme/custom_text_style.dart';
+export 'package:healt_care_app/theme/theme_helper.dart';
+export 'package:healt_care_app/widgets/custom_image_view.dart';
+export '../theme/custom_button_style.dart';
+export 'package:healt_care_app/core/utils/date_time_utils.dart';
